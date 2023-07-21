89208e93e28398030aa2051c4df83c634568ac96
Mellerh <103604450+Mellerh@users.noreply.github.com>
2023-07-21 11:31:50 +0300

    chanched add a basename to BrowserRouter in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
     return(
         <>
-            <BrowserRouter>
+            <BrowserRouter basename='/Project_7_React_SPA_food'>
 
                 <Header />
                 
